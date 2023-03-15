dfc74ea14a4a70304f621dc5c424aa048a23c55d
Owenlin <owenlin1604@gmail.com>
2023-03-15 08:46:08 -0400

    Model and scenebuilder

--- src/main/java/com/example/labsignin/HelloController.java
+++ src/main/java/com/example/labsignin/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.labsignin;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.io.FileNotFoundException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public ImageView portrait;
+    public ComboBox listOfNames;
+    public Label signedIn;
+    public Button signIn;
+
+    public void initialize() throws FileNotFoundException {
+
+
     }
-}
+
+    }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.labsignin {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.labsignin to javafx.fxml;
     exports com.example.labsignin;
 }
