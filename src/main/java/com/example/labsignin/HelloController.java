cd9db92fadf831f3ce360de04cce8472b499b96b
Owenlin <owenlin1604@gmail.com>
2023-03-20 14:50:49 -0400

    Model and scenebuilder

--- src/main/java/com/example/labsignin/HelloController.java
+++ src/main/java/com/example/labsignin/HelloController.java
@@ -1,21 +1,23 @@
 package com.example.labsignin;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 
 import java.io.FileNotFoundException;
 
 public class HelloController {
 
-    public ImageView portrait;
-    public ComboBox listOfNames;
-    public Label signedIn;
+    public TextField nameField;
+    public ComboBox bandList;
     public Button signIn;
-    public TextField autoTextField;
+    public ImageView studentPortrait;
+    public TextField email;
+    public TableView infoList;
+    public ComboBox cteList;
+    public ComboBox gradeList;
+    public ComboBox studentList;
+    public ComboBox imageList;
 
 
     public void initialize() throws FileNotFoundException {
