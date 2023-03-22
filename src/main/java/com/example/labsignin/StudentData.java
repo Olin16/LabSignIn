7016a8a4de614844285a7b39988383b42fcaec1a
Owenlin <owenlin1604@gmail.com>
2023-03-22 09:11:14 -0400

    Model and scenebuilder

--- src/main/java/com/example/labsignin/StudentData.java
+++ src/main/java/com/example/labsignin/StudentData.java
@@ -1,4 +1,43 @@
 package com.example.labsignin;
 
+import javafx.scene.image.Image;
+
+
 public class StudentData {
+    private String cte;
+    private Integer grade;
+    private String name;
+    private Image image;
+
+    public String getCte() {
+        return cte;
+    }
+
+    public void setCte(String cte) {
+        this.cte = cte;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
 }
