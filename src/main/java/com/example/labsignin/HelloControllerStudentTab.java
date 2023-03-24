f7751331cf9727bc618501fe0988ecb6734dfbca
samgombiner <sam.gombiner@gmail.com>
2023-03-24 13:03:19 -0400

    Two Controllers

---
+++ src/main/java/com/example/labsignin/HelloControllerStudentTab.java
@@ -0,0 +1,22 @@
+package com.example.labsignin;
+
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
+import java.io.FileNotFoundException;
+
+public class HelloControllerStudentTab extends HelloControllerTorianTab{
+
+    public TextField nameField;
+    public ComboBox bandList;
+    public Button signIn = new Button("Sign In");
+    public ImageView studentPortrait;
+
+
+
+    public void initialize() throws FileNotFoundException {
+
+
+    }
+
+    }
--- src/main/java/com/example/labsignin/HelloControllerTorianTab.java
+++ src/main/java/com/example/labsignin/HelloControllerTorianTab.java
@@ -1,29 +1,26 @@
 package com.example.labsignin;
 
-import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 
 import java.io.FileNotFoundException;
 
-public class HelloController {
-
-    public TextField nameField;
-    public ComboBox bandList;
-    public Button signIn = new Button("Sign in");
-    public ImageView studentPortrait;
+public class HelloControllerTorianTab {
     public TextField email;
+    public PasswordField password;
     public TableView infoList;
     public ComboBox cteList;
     public ComboBox gradeList;
     public ComboBox studentList;
     public ComboBox imageList;
-    public PasswordField password;
-
-
     public void initialize() throws FileNotFoundException {
 
 
     }
 
+    public PasswordField getPassword() {
+        System.out.println(password.getAccessibleText());
+        return password;
+
     }
+}
